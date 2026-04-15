f1e6904f638ce26826c9303126706abea288536d
Bromu162 <bromu162@gmail.com>
2026-04-15 19:12:23 +0200

    Inicjalizacja głównych endpointów REST API

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,21 @@
-// Importowanie wymaganych modułów
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
-// Inicjalizacja aplikacji Express
+
+connectDB();
+
 const app = express();
 
-// Middlewares
-app.use(cors()); 
-app.use(express.json()); 
+
+app.use(cors());
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-  res.send('API Systemu Rezerwacji Boisk działa! ⚽');
-});
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/facilities', require('./routes/facilityRoutes')); 
+app.use('/api/reservations', require('./routes/reservationRoutes')); 
 
-// Konfiguracja portu
 const PORT = process.env.PORT || 5000;
-
-// Połączenie z bazą MongoDB i start serwera
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('✅ Połączono z bazą danych MongoDB');
-    app.listen(PORT, () => {
-      console.log(`🚀 Serwer śmiga na porcie ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('❌ Błąd połączenia z bazą MongoDB:', err.message);
-  });
+app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
